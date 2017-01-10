a6f197d35cf96454c31f81bdead92def382dfab4
Jonny Dalgleish <fighella@gmail.com>
2017-01-10 12:55:43 +1100

    More Pages

--- lib/dropbox_product_updates/product_data.rb
+++ lib/dropbox_product_updates/product_data.rb
@@ -89,7 +89,7 @@ class ProductData
       username: 'Data Notifier', icon_url: 'https://cdn.shopify.com/s/files/1/1290/9713/t/4/assets/favicon.png?3454692878987139175'
 
     shopify_variants = []
-    [1,2,3].each do |page|
+    [1,2,3,4,5].each do |page|
       shopify_variants << ShopifyAPI::Variant.find(:all, params: { limit: 250, fields: 'sku, product_id', page: page } )
     end
     shopify_variants = shopify_variants.flatten
